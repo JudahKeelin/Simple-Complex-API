05e974c21b66d924163e0920f8502f4cb28c6a2f
Judah Keelin <111410965+JudahKeelin@users.noreply.github.com>
2023-06-23 18:43:06 -0500

    bug fix

--- SimpleComplex/src/main/java/com/example/SimpleComplex/Repository/UserInfoRepository.java
+++ SimpleComplex/src/main/java/com/example/SimpleComplex/Repository/UserInfoRepository.java
@@ -2,6 +2,10 @@ package com.example.SimpleComplex.Repository;
 import com.example.SimpleComplex.Records.UserInfo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserInfoRepository extends CrudRepository<UserInfo, Integer> {
     UserInfo findByEmailAndPassword(String email, String password);
+
+    UserInfo findByUUID(int id);
 }
